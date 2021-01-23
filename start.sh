5a41ebd87086a134c76ece21caa9260edcbd7dcc
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-23 01:15:02 -0400

    added a bash file to have an easy start

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source venv/bin/activate
+pip install -r requierements.txt
+source venv/bin/activate
+
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+export FLASK_ENV=development
+
+flask run
