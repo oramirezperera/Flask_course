aae33bcbc552d41d687ffcc3b8552abbd4055584
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-26 21:42:46 -0400

    added a form and now displays the username capitalized with title and the IP

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, make_response, redirect, render_template, session  
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,6 +11,10 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Buy coffe', 'Send the buy request', 'Send the product']
 
+class LoginForm(FlaskForm):
+    username = StringField('User name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Send')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -29,13 +36,23 @@ def index():
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip': user_ip, 
         'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
