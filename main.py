4d6dc89c2d525b27e3eb58010e87dc19b472df7f
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-26 22:47:22 -0400

    created tests folder where all the tests will be created, creating test for the app changed requirements.txt added flask_testing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -15,6 +16,13 @@ class LoginForm(FlaskForm):
     username = StringField('User name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Send')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.errorhandler(404)
 def not_found(error):
