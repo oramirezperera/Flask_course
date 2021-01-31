1f21e1942f091dea2a865b8e5f3e5a50911b2c00
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-31 11:59:07 -0400

    major changes to the app, now thers no login form in hello, and all the logins pas through views, main.py changed to get this changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,20 @@
-from flask import render_template
+from flask import render_template, session, url_for, flash, redirect
 from app.forms import LoginForm
 from . import auth
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    login_form = LoginForm()
     context = {
-        'login_form': LoginForm()
+         'login_form': login_form
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('successfully added the user')
+
+        return redirect(url_for('index'))
 
     return render_template('login.html', **context)
--- tests/test_base.py
+++ tests/test_base.py
@@ -26,13 +26,9 @@ class MainTest(TestCase):
         self.assert200(response)
 
     def test_hello_post(self):
-        fake_form = {
-            'username': 'fake',
-            'password': 'fake_password'
-        }
-        response = self.client.post(url_for('hello'), data=fake_form)
+        response = self.client.post(url_for('hello'))
 
-        self.assertRedirects(response, url_for('index'))
+        self.assertTrue(response.status_code, 405)
 
     def test_auth_blueprint_exists(self):
         self.assertIn('auth', self.app.blueprints)
@@ -46,3 +42,12 @@ class MainTest(TestCase):
         self.client.get(url_for('auth.login'))
 
         self.assertTemplateUsed('login.html')
+
+    def test_auth_login_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake_password'
+        }
+
+        response = self.client.post(url_for('auth.login'), data=fake_form)
+        self.assertRedirects(response, url_for('index'))
