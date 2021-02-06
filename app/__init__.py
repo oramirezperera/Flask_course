9d3187d0e1e509890e2400bba4bf814666d56013
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-02-06 16:36:42 -0400

    added mysql as a database to the project

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
@@ -9,7 +11,17 @@ def create_app():
     bootstrap = Bootstrap(app)
 
     app.config.from_object(Config)
+    app.config.from_mapping(
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+    )
     
     app.register_blueprint(auth)
+
+    from . import db
+
+    db.init_app(app)
 
     return app
--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+import os
+
 class Config():
     SECRET_KEY = 'SUPER SECRET'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
+    
